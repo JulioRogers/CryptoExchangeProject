9c46c9a9914908d51680035d7df68a75ffd5fadd
JulioRogers <juliorogers.cg@gmail.com>
2024-08-24 16:42:05 -0500

    create user attributes

--- src/main/java/com/globant/model/User.java
+++ src/main/java/com/globant/model/User.java
@@ -1,4 +1,15 @@
 package com.globant.model;
 
 public class User {
+    private final String uniqueID;
+    private final String name;
+    private String email;
+    private String password;
+
+    public User(String uniqueID, String name, String email, String password) {
+        this.uniqueID = uniqueID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
