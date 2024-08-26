75c5d3a7c6459d12b4f3d7d780b064045b9c3113
JulioRogers <juliorogers.cg@gmail.com>
2024-08-25 23:00:58 -0500

    create logout method

--- src/main/java/com/globant/controller/UserSystemController.java
+++ src/main/java/com/globant/controller/UserSystemController.java
@@ -1,7 +1,9 @@
 package com.globant.controller;
 
+import com.globant.model.User;
 import com.globant.service.DuplicateEmailException;
 import com.globant.service.InvalidEmailFormatException;
+import com.globant.service.SessionService;
 import com.globant.service.UsersSystemService;
 import com.globant.storage.UnknownAccountException;
 import com.globant.view.ConsoleView;
@@ -9,10 +11,12 @@ import com.globant.view.ConsoleView;
 public class UserSystemController {
     private final UsersSystemService usersSystemService;
     private final ConsoleView view;
+    private final SessionService sessionService;
 
-    public UserSystemController(ConsoleView view, UsersSystemService usersSystemService) {
+    public UserSystemController(ConsoleView view, UsersSystemService usersSystemService, SessionService sessionService) {
         this.view = view;
         this.usersSystemService = usersSystemService;
+        this.sessionService = sessionService;
     }
 
     public void createUser() {
@@ -36,6 +40,8 @@ public class UserSystemController {
         try{
             boolean isValid = usersSystemService.validateUser(email, password);
             if(isValid){
+                User user = usersSystemService.getUserbyEmail(email);
+                sessionService.login(user);
                 view.showSuccessMessage("User successfully logged in");
             } else{
                 view.showError("Incorrect Password");
@@ -43,5 +49,9 @@ public class UserSystemController {
         } catch (InvalidEmailFormatException | UnknownAccountException e){
             view.showError(e.getMessage());
         }
+    }
+
+    public void logout() {
+        sessionService.logout();
     }
 }
