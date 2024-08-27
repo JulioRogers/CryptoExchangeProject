da4a70d4983f1749ece74e73eb31679ce7671caa
JulioRogers <juliorogers.cg@gmail.com>
2024-08-26 21:32:43 -0500

    create Wallet object in User Constructor

--- src/main/java/com/globant/model/User.java
+++ src/main/java/com/globant/model/User.java
@@ -5,12 +5,18 @@ public class User {
     private final String name;
     private final String email;
     private final String password;
+    private final Wallet wallet;
 
     public User(Integer uniqueID, String name, String email, String password) {
         this.uniqueID = uniqueID;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.wallet = new Wallet(uniqueID);
+    }
+
+    public Wallet getWallet() {
+        return wallet;
     }
 
     public String getEmail() {
