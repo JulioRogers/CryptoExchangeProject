92cd52934c19c046fb6fe919d5b996b6706b9686
JulioRogers <juliorogers.cg@gmail.com>
2024-08-29 15:41:43 -0500

    rename Main.java

--- src/main/java/com/globant/CryptoExchangeApp.java
+++ src/main/java/com/globant/CryptoExchangeApp.java
@@ -8,7 +8,7 @@ import com.globant.view.ConsoleView;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class CryptoExchangeApp {
     public static void main(String[] args) {
         SessionService sessionService = new SessionService();
         ConsoleView view = new ConsoleView();
