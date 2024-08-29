976073309805a533dbb1f0fcd9e384ebc9124ab6
JulioRogers <juliorogers.cg@gmail.com>
2024-08-29 01:32:39 -0500

    remove InMemoryUsersStorage.java, it was not correctly created

--- src/main/java/com/globant/model/UsersStorage.java
+++
@@ -1,8 +0,0 @@
-package com.globant.model;
-
-public interface UsersStorage {
-    void saveUser(Integer uniqueId, User user);
-    User getUserByEmail(String email);
-    User getUserById(Integer id);
-    Integer generateUniqueId();
-}
