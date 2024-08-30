9d4a04bf217d48e8ecefacebc55af2136e4b1000
JulioRogers <juliorogers.cg@gmail.com>
2024-08-30 14:43:10 -0500

    add user in order attributes

--- src/main/java/com/globant/model/orders/BuyOrder.java
+++ src/main/java/com/globant/model/orders/BuyOrder.java
@@ -1,10 +1,12 @@
 package com.globant.model.orders;
 
+import com.globant.model.User;
+
 import java.math.BigDecimal;
 
 public class BuyOrder extends Order {
 
-    public BuyOrder(String crypto, BigDecimal amount, BigDecimal price){
-        super(crypto, amount, price);
+    public BuyOrder(User user, String crypto, BigDecimal amount, BigDecimal price){
+        super(user, crypto, amount, price);
     }
 }
--- src/main/java/com/globant/model/orders/SellOrder.java
+++ src/main/java/com/globant/model/orders/SellOrder.java
@@ -1,10 +1,12 @@
 package com.globant.model.orders;
 
+import com.globant.model.User;
+
 import java.math.BigDecimal;
 
 public class SellOrder extends Order {
 
-    public SellOrder(String crypto, BigDecimal amount, BigDecimal price) {
-        super(crypto, amount, price);
+    public SellOrder(User user, String crypto, BigDecimal amount, BigDecimal price) {
+        super(user, crypto, amount, price);
     }
 }
