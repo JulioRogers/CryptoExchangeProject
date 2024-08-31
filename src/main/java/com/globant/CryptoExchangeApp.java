980ead678e2aef18eb4d958e00a27c0ae45d8aaa
JulioRogers <juliorogers.cg@gmail.com>
2024-08-31 16:16:36 -0500

    clean comments

--- src/main/java/com/globant/CryptoExchangeApp.java
+++ src/main/java/com/globant/CryptoExchangeApp.java
@@ -5,8 +5,6 @@ import com.globant.service.SessionService;
 import com.globant.view.ConsoleView;
 import com.globant.view.View;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class CryptoExchangeApp {
     public static void main(String[] args) {
         SessionService sessionService = new SessionService();
