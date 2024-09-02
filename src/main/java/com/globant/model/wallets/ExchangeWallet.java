55065162f5ab1d55ba2fc37d5589f27292c1417f
JulioRogers <juliorogers.cg@gmail.com>
2024-09-02 01:39:12 -0500

    document wallets

--- src/main/java/com/globant/model/wallets/ExchangeWallet.java
+++ src/main/java/com/globant/model/wallets/ExchangeWallet.java
@@ -5,7 +5,21 @@ import com.globant.model.currencies.Currency;
 
 import java.math.BigDecimal;
 
-public class ExchangeWallet extends Wallet {
+/**
+ * The {@code ExchangeWallet} class is a specialized wallet designed to handle
+ * cryptocurrency transactions exclusively. It extends the {@code Wallet} class
+ * to leverage the existing code for receiving and delivering currencies, thus
+ * avoiding the repetition of business logic.
+ *
+ * <p>This class ensures that only cryptocurrencies are processed by overriding
+ * the {@code deliverCurrency} and {@code receiveCurrency} methods with checks
+ * that enforce this rule.</p>
+ *
+ * <p>The main purpose of this class is to reuse the code from the {@code Wallet}
+ * class for handling currency transactions, while applying specific restrictions
+ * pertinent to the project requirements.</p>
+ */
+ public class ExchangeWallet extends Wallet {
     @Override
     public void deliverCurrency(Currency currency, BigDecimal amount) {
         if (!(currency instanceof CryptoCurrency)) {
--- src/main/java/com/globant/model/wallets/Wallet.java
+++ src/main/java/com/globant/model/wallets/Wallet.java
@@ -14,14 +14,38 @@ public abstract class Wallet {
         this.currencyBalances = new HashMap<>();
     }
 
+    /**
+     * Transfers a specified amount of the given currency from the user's balance.
+     *
+     * @param currency The {@code Currency} to be delivered.
+     * @param amount The amount of currency to be delivered.
+     */
     public void deliverCurrency(Currency currency, BigDecimal amount) {
         subtractToBalance(currency,amount,this.currencyBalances);
     }
 
+    /**
+     * Adds a specified amount of the given currency to the user's balance.
+     *
+     * @param currency The {@code Currency} to be received.
+     * @param amount The amount of currency to be received.
+     */
     public void receiveCurrency(Currency currency, BigDecimal amount) {
         addToBalance(currency,amount,this.currencyBalances);
     }
 
+    /**
+     * Subtracts a specified amount of the given currency from the balance.
+     *
+     * <p>This method is protected so it can be reused by the {@code UserWallet} subclass when
+     * operating on the frozen balance, promoting code reuse.</p>
+     *
+     * @param currency The {@code Currency} to subtract from the balance.
+     * @param amount The amount of currency to be subtracted.
+     * @param balance A {@code Map<Currency, BigDecimal>} representing the balance from which the amount will be subtracted.
+     *
+     * @throws IllegalArgumentException if the amount exceeds the current balance.
+     */
     protected void subtractToBalance(Currency currency, BigDecimal amount, Map<Currency, BigDecimal> balance) {
         BigDecimal currentBalance = getBalance(currency, balance);
         fundsValidation(amount, currentBalance);
@@ -29,6 +53,16 @@ public abstract class Wallet {
         balance.put(currency, newBalance);
     }
 
+    /**
+     * Adds a specified amount of the given currency to the balance.
+     *
+     * <p>This method is protected so it can be reused by the {@code UserWallet} subclass when
+     * operating on the frozen balance, promoting code reuse.</p>
+     *
+     * @param currency The {@code Currency} to add to the balance.
+     * @param amount The amount of currency to be added.
+     * @param balance A {@code Map<Currency, BigDecimal>} representing the balance to which the amount will be added.
+     */
     protected void addToBalance(Currency currency, BigDecimal amount, Map<Currency, BigDecimal> balance) {
         BigDecimal currentBalance = getBalance(currency, balance);
         BigDecimal newBalance = currentBalance.add(amount);
